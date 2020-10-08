ec145076ebe3b86ac70e39a3ab2291c6df121fc9
angel daniel <daniel_1234@hotmail.es>
2020-10-08 15:11:47 -0400

    estructura de servicios

--- src/Components/Administration/Services/index.js
+++ src/Components/Administration/Services/index.js
@@ -1,14 +1,143 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, Container, Row, Col, ButtonGroup, Button as BB, Modal, Form } from 'react-bootstrap';
+import { Button, Spinner, Callout, Alert, Icon } from "@blueprintjs/core";
 
+import { API, graphqlOperation } from 'aws-amplify';
+
+import { deleteCompanyService } from '../../../graphql/mutations';
 import useServices from './useServices'
 
 const Services = (props) => {
+    const [show, setShow] = useState(false);
+    const [edit, setEdit] = useState(false);
+
+    const [ so, setSelectedObject ] = useState({
+        service: {
+            name: ''
+        }
+    });
+
+    const [ service, setService ] = useState('');
+    const [ cost, setCost ] = useState('');
+
+
     const {} = useServices();
+
+
+    const handleClose = () => setShow(false);
+
+    const handleShow = (action, object) => {
+        setSelectedObject(object);
+        setCost(object.cost)
+        setEdit(action === 'edit' ? true : false);
+        setShow(true);
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            var list = props.ap.cser.companyServices;
+            const api = await API.graphql(graphqlOperation(deleteCompanyService, {input: {id: id}}));
+            list.splice(list.findIndex(e => e.id === id), 1);
+            
+            props.ap.cser.setCompanyServices(list);
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const handleAddService = async () => {
+        console.log(service);
+    }
+
+    useEffect(() => {
+		let didCancel = false;
+        console.log(props.ap);
+		return () => {
+			didCancel = true;
+		};
+	}, []);
     
-    return(<div>
-        <h5>Servicios </h5>
-        <button >test</button>
-    </div>)
+    const list = (props.ap.cser.companyServices !== null)?([].concat(props.ap.cser.companyServices)
+		.map((item,i)=>
+			(
+				<tr key={i}>
+					{/* <td>{i+1}</td> */}
+					<td style={{width: 200}}>{item.service.name}</td>
+					<td>
+                        <ButtonGroup size="sm">
+                            <BB onClick={e => { e.preventDefault(); handleShow('view', item);}} ><Icon icon="eye-open"/></BB>
+                            <BB onClick={e => { e.preventDefault(); handleShow('edit', item);}} ><Icon icon="edit"/></BB>
+                            <BB onClick={e => { e.preventDefault(); handleDelete(item.id);}} ><Icon icon="trash"/></BB>
+                        </ButtonGroup>
+                    </td>
+				</tr>
+			)
+        )):(<td></td>)
+
+    const servicesList = (props.ap.ser.services !== null)?([].concat(props.ap.ser.services)
+	    .map((item,i)=>
+		(
+            <option key={i} value={item.id}>{item.name}</option>
+		)
+     )):(<td></td>)
+
+    if(props.ap.err.error.type === 'services') return(<div style={{marginTop: 5}}><h5>{props.ap.err.error.message}</h5></div>)
+    
+    if(props.ap.load.loading.type === 'services') return(<Spinner/>)
+        
+    return(<Container fluid>
+        <Row>
+            <Col sm={4}>
+                <Form.Group>
+                    <Form.Control as="select" id="services" onChange={e => setService(e)}>
+                        <option>Seleccione...</option>
+                        {servicesList}
+                    </Form.Control>
+                </Form.Group>
+            </Col>
+            <Col sm={2}><Button intent="Primary" onClick={(e) => {e.preventDefault(); handleAddService();}} icon="add"></Button></Col>
+        </Row>
+        <div style={{marginTop:20}}>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						{/* <th>No.</th> */}
+						<th>Servicio</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					{list}
+				</tbody>
+			</Table>
+		</div>
+
+        {/* Modal para editar y ver detalle de servicios */}
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{edit ? 'Editar Servicio' : 'Ver Servicio'}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group controlId="name">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control readOnly={true} type="text" value={so.service.name} />
+                </Form.Group>
+                <Form.Group controlId="cost">
+                    <Form.Label>Costo</Form.Label>
+                    <Form.Control readOnly={!edit} type="text" value={cost}/>
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Cerrar
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Guardar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </Container>)
 }
 
 export default Services;
--- src/Components/Administration/useAdministration.js
+++ src/Components/Administration/useAdministration.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { updateRequest } from '../../graphql/mutations';
+import { getCompanyProductsAndServices } from '../../graphql/customQueries';
 import { listServices, listProducts, listOffices, listRequests, listEmployees} from '../../graphql/queries';
 
 const useAdministration = (props) => {
@@ -10,6 +11,7 @@ const useAdministration = (props) => {
     const [ offices, setOffices ] = useState([]);
     const [ services, setServices ] = useState([]);
     const [ products, setProducts ] = useState([]);
+    const [ employees, setEmployees ] = useState([]);
 	
 	const [ loading, setLoading ] = useState({
 		type: ''
@@ -45,10 +47,11 @@ const useAdministration = (props) => {
 				_offices();
 				break;
 			case 'services':
-				_companyServices();
+				_getCompanyData(e);
+				_services();
 				break;
 			case 'products':
-				_companyProducts();
+				_getCompanyData(e);
 				break;
 			case 'employees':
 				_employees();
@@ -62,10 +65,12 @@ const useAdministration = (props) => {
 	//solo se podra llamar cuando se necesite crear o editar un servicio de la compania
 	const _services = async () => {
 		try {
-			setLoading({type: 'services'})
-			const api = await API.graphql(graphqlOperation(listServices));
-			setServices(api.data.listServices.items);
-			setLoading({type: ''})
+			if(services.length === 0){
+				//setLoading({type: 'services'})
+				const api = await API.graphql(graphqlOperation(listServices));
+				setServices(api.data.listServices.items);
+				setLoading({type: ''})
+			}
 		} catch (e) {
 			setError({
 				type: 'services',
@@ -76,27 +81,91 @@ const useAdministration = (props) => {
 	}
 
 	const _products = async () => {
-
+		try {
+			if(products.length === 0){
+				setLoading({type: 'products'})
+				const api = await API.graphql(graphqlOperation(listProducts));
+				setProducts(api.data.listProducts.items);
+				setLoading({type: ''})
+			}
+		} catch (e) {
+			setError({
+				type: 'products',
+				message: 'Error al buscar los productos'
+			})
+			setLoading({type: ''})
+		}
 	}
 
 	const _requests = async () => {
-
+		try {
+			if(requests.length === 0){
+				setLoading({type: 'requests'})
+				const api = await API.graphql(graphqlOperation(listRequests));
+				setRequests(api.data.listRequests.items);
+				setLoading({type: ''})
+			}
+		} catch (e) {
+			setError({
+				type: 'requests',
+				message: 'Error al buscar las solicitudes'
+			})
+			setLoading({type: ''})
+		}
 	}
 
 	const _offices = async () => {
-
+		try {
+			if(offices.length === 0){
+				setLoading({type: 'offices'})
+				const api = await API.graphql(graphqlOperation(listOffices));
+				setOffices(api.data.listOffices.items);
+				setLoading({type: ''})
+			}
+		} catch (e) {
+			setError({
+				type: 'offices',
+				message: 'Error al buscar las oficinas'
+			})
+			setLoading({type: ''})
+		}
 	}
 
-	const _companyServices = async () => {
-
-	}
-
-	const _companyProducts = async () => {
+	const _getCompanyData = async (type) => {
+		try {
+			if(companyProducts.length === 0 || companyServices.length === 0){
+				setLoading({type: type})
+				const api = await API.graphql(graphqlOperation(getCompanyProductsAndServices, {id: props.state.company.id}));
+				setCompanyProducts(api.data.getCompany.products.items);
+				setCompanyServices(api.data.getCompany.services.items);
+				setLoading({type: ''})
+			}
+		} catch (e) {
+			console.log(e);
+			setError({
+				type: type,
+				message: 'Error al buscar los datos de la compania'
+			})
+			setLoading({type: ''})
+		}
 
 	}
 
 	const _employees = async () => {
-
+		try {
+			if(employees.length === 0){
+				setLoading({type: 'employees'})
+				const api = await API.graphql(graphqlOperation(listEmployees));
+				setEmployees(api.data.listEmployees.items);
+				setLoading({type: ''})
+			}
+		} catch (e) {
+			setError({
+				type: 'employees',
+				message: 'Error al buscar los empleados'
+			})
+			setLoading({type: ''})
+		}
 	}
 
 
@@ -158,7 +227,7 @@ const useAdministration = (props) => {
 		});
 	}
 
-	return { ap, onSelectTab, requests, cancelRequest, cancelOverlay, cancelLoading, cancelerror, cancelerrorMessage, confirmCancelRequest, setCancelOverlay, loading, errorMessage };
+	return { ap, onSelectTab, requests, cancelRequest, cancelOverlay, cancelLoading, cancelerror, cancelerrorMessage, confirmCancelRequest, setCancelOverlay };
 };
 
 export default useAdministration;
